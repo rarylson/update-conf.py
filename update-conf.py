02d7283b54284fb07009c4fa9a09bf723e374892
Rarylson Freitas <rarylson@vialink.com.br>
2015-02-24 01:27:03 -0300

    refactoring the script.
    now, it's can be used in Python2.6, Python3, it uses tempfile,
    SafeConfigParser, and the code is much more clean.

--- update-conf.py
+++ update-conf.py
@@ -8,109 +8,117 @@ NOTE: This software was based in the 'update-conf.d' project.
       See: https://github.com/Atha/update-conf.d
 """
 
+from __future__ import print_function
+
 import sys
 import os
 import shutil
-import re
 import subprocess
+from tempfile import mkstemp
 
 import argparse
-import ConfigParser
+from ConfigParser import SafeConfigParser
 
 # About
 __author__ = "Rarylson Freitas"
 __email__ = "rarylson@gmail.com"
 __version__ = "0.1.0"
 
-# Constants
-# Program
-PROGRAM = sys.argv[0]
-PROGRAM_NAME = os.path.basename(PROGRAM)
-WORKDIR = os.path.dirname(PROGRAM)
-# Default arguments
-DEFAULT_CONFIG = PROGRAM_NAME + ".conf"
-# XXX 'conf.d' or 'snmpd.conf.d'? What is the best default name???
-DEFAULT_SPLIT_DIR = "conf.d"
-# Aux files
-TEMP_END = ".tmp"
-BACKUP_END = ".bak"
-# Files in the 'conf.d' like dir that will be ignored 
-IGNORE_FILES_PATTERN = "\.[bak|backup|old|inactive]"
+# Consts
+DEFAULT_CONFIG = "/etc/update-conf.py.conf"
+DEFAULT_DIR_EXT = "d"
+IGNORE_FILES_EXT = ["bak", "backup", "old", "inactive"]
+BACKUP_EXT = "bak"
 
-# Usage
-usage_string = """{prog_name} [-h] -n NAME [-c CONF]
-         or
-       {prog_name} [-h] -f FILE [-d DIR] [-m MAIN]""".format(prog_name=PROGRAM)
 
-# Parsing args
-parser = argparse.ArgumentParser(usage=usage_string, 
-        description="Generate config files from 'conf.d' like directories")
-group = parser.add_mutually_exclusive_group()
-group.required = True
-# Use with command name
-group.add_argument("-n", "--name", help="name of a previously defined command")
-parser.add_argument("-c", "--conf", help="update-conf.d conf file (default update-conf.py.conf)", 
-        default=DEFAULT_CONFIG)
-# Use with file name
-group.add_argument("-f", "--file", help="configuration file to be generated")
-parser.add_argument("-d", "--dir", help="directory where the splitted files are found (default FILE_DIR/conf.d)")
-parser.add_argument("-m", "--main", help="main configuration file")
+# Parse args and config file
+
+# Parse args
+parser = argparse.ArgumentParser(
+    description="Generate config files from 'conf.d' like directories")
+parser.add_argument(
+    "-f", "--file", help="config file to be generated")
+parser.add_argument(
+    "-d", "--dir",
+    help="directory whith the splitted files (default FILE_PATH/FILE_NAME.{0})".format(
+        DEFAULT_DIR_EXT))
+parser.add_argument(
+    "-n", "--name",
+    help="name of the section (defined in the config file) to be used while generating a "
+    "config file")
+parser.add_argument(
+    "-c", "--config",
+    help="update-conf.py config file (default {0})", default=DEFAULT_CONFIG)
+parser.add_argument(
+    "-v", "--version", action="version", version=__version__)
 args = parser.parse_args()
 
-# Parsing config file
-# See: http://docs.python.org/2/library/configparser.html#module-ConfigParser
+# Parse config file
 if args.name:
-    config_parser = ConfigParser.RawConfigParser()
-    # Open config file 
+    config_parser = SafeConfigParser()
+    # Open config file
     try:
-        config_parser.readfp( open(args.conf) )
+        config_parser.readfp(open(args.config, 'r'))
     except IOError:
-        print "usage: {}".format(usage_string)
-        print "{}: error: config file '{}' not found".format(PROGRAM_NAME, args.conf)
-        sys.exit(1)
-    # Errors
-    if not config_parser.has_section( args.name ):
-        print "{}: error: invalid command name '{}'".format(PROGRAM_NAME, args.name)
-        sys.exit(1)
-    if not config_parser.has_option( args.name, 'file' ):
-        print "{}: error: required value 'file' not found in {}, section '{}'".format(
-                PROGRAM_NAME, args.conf, args.name)
-        sys.exit(1)
-    # Setting values
-    # 'file' is required
-    args.file = config_parser.get(args.name,'file')
-    if config_parser.has_option(args.name,'dir'):
-        args.dir = config_parser.get(args.name,'dir')
-    if config_parser.has_option(args.name,'main'):
-        args.main = config_parser.get(args.name,'main')
+        parser.error("config file '{}' not found".format(args.config))
+    # Section not found error
+    if not config_parser.has_section(args.name):
+        parser.error("section name '{}' not found in config file".format(args.name))
+    # Get options from config file (options from command line take precedence)
+    if not args.file and config_parser.has_option(args.name, 'file'):
+        args.file = config_parser.get(args.name, 'file')
+    if not args.dir and config_parser.has_option(args.name, 'dir'):
+        args.dir = config_parser.get(args.name, 'dir')
 
-# 'args.dir' default value
+# Required 'file' error
+if not args.file:
+    parser.error("'file' is required (you must set it via config file or cmd arg)")
+
+# Default value of 'dir'
 if not args.dir:
-    args.dir = os.path.dirname(args.file) + os.sep + DEFAULT_SPLIT_DIR
+    args.dir = "{0}.{1}".format(args.file, DEFAULT_DIR_EXT)
 
-# Generating the new config file
-temp_file = os.path.abspath(args.file) + TEMP_END
-# For prevent wrong result after a previous bug, remove the old temp file
-if os.path.isfile(temp_file):
-    os.remove(temp_file)
-# TODO Add comment pattern as an option (#,;,//)
-#      subprocess.call("echo '# Auto-generated by update-conf.py' > {}".format(temp_file), shell=True)
-# Merge main file
-if args.main:
-    subprocess.call("cat {} >> {}".format(args.main, temp_file), shell=True)
-# Merge files in the 'conf.d' like dir
-# Get files that not end with '.bak', '.old' and other terminations sorted by lexical order
-split_files = [args.dir + os.sep + split_file for split_file in os.listdir(args.dir) if 
-        not re.search(IGNORE_FILES_PATTERN, split_file)]
-# Merge these files
-for split_file in split_files:
-    subprocess.call("cat {} >> {}".format(split_file, temp_file), shell=True)
 
-# Updating the new config file
-# Backup current config file
-backup_file = os.path.abspath(args.file) + BACKUP_END
+# Get all splitted files
+splitted_files = []
+try:
+    for entry in os.listdir(args.dir):
+        entry_path = os.path.join(args.dir, entry)
+        entry_is_valid = True
+        if not os.path.isfile(entry_path):
+            continue
+        for ext in IGNORE_FILES_EXT:
+            if entry.endswith(ext):
+                entry_is_valid = False
+                break
+        if entry_is_valid:
+            splitted_files.append(entry_path)
+except OSError:
+    print("Error: Dir '{0}' not found".format(args.dir))
+    sys.exit(1)
+# No splitted file error
+if not splitted_files:
+    print("Error: No splitted files found in dir '{0}'".format(args.dir))
+    sys.exit(1)
+
+
+# Generate new config file
+
+# Generate in a temp file
+temp_file_fd, temp_file = mkstemp()
+# Convert 'temp_file_fd' to a Python file object
+# See: http://www.logilab.org/blogentry/17873
+temp_file_fd = os.fdopen(temp_file_fd, 'w')
+for splitted in splitted_files:
+    with open(splitted, 'r') as splitted_fd:
+        temp_file_fd.write(splitted_fd.read())
+temp_file_fd.close()
+
+# Backup the old config file
 if os.path.isfile(args.file):
-    shutil.move(args.file, backup_file)
-# Updating current config file
+    os.rename(args.file, "{0}.{1}".format(args.file, BACKUP_EXT))
+
+# Use the new config file
+# Using shutil.move because the tmp file can be in a different filesystem
 shutil.move(temp_file, args.file)
 
