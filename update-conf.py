9475e8bbc45bb9fa18a0914bf0b79292baf6dc51
Rarylson Freitas <rarylson@vialink.com.br>
2015-02-25 00:54:47 -0300

    refactoring: breaking the code into smaller functions.
    the code was changed to be pep8 compliant too.

--- update-conf.py
+++ update-conf.py
@@ -13,8 +13,7 @@ from __future__ import print_function
 import sys
 import os
 import shutil
-import subprocess
-from tempfile import mkstemp
+import tempfile
 
 import argparse
 from ConfigParser import SafeConfigParser
@@ -31,94 +30,142 @@ IGNORE_FILES_EXT = ["bak", "backup", "old", "inactive"]
 BACKUP_EXT = "bak"
 
 
-# Parse args and config file
+# Parse command line args and config file
+#
+# The config file to be parsed is the program default or passed via command
+# line args.
+# An error message and the usage will be printed and the program will exit if
+# any parser error occur.
+# It returns a argparse 'args' object.
+def _parse_all():
+    args = None
 
-# Parse args
-parser = argparse.ArgumentParser(
-    description="Generate config files from 'conf.d' like directories")
-parser.add_argument(
-    "-f", "--file", help="config file to be generated")
-parser.add_argument(
-    "-d", "--dir",
-    help="directory whith the splitted files (default FILE_PATH/FILE_NAME.{0})".format(
-        DEFAULT_DIR_EXT))
-parser.add_argument(
-    "-n", "--name",
-    help="name of the section (defined in the config file) to be used while generating a "
-    "config file")
-parser.add_argument(
-    "-c", "--config",
-    help="update-conf.py config file (default {0})", default=DEFAULT_CONFIG)
-parser.add_argument(
-    "-v", "--version", action="version", version=__version__)
-args = parser.parse_args()
+    # Parse args
+    parser = argparse.ArgumentParser(
+        description="Generate config files from 'conf.d' like directories")
+    parser.add_argument(
+        "-f", "--file", help="config file to be generated")
+    parser.add_argument(
+        "-d", "--dir",
+        help="directory whith the splitted files (default "
+             "FILE_PATH/FILE_NAME.{0})".format(DEFAULT_DIR_EXT))
+    parser.add_argument(
+        "-n", "--name",
+        help="name of the section (defined in the config file) to be used "
+             "while generating a config file")
+    parser.add_argument(
+        "-c", "--config",
+        help="update-conf.py config file (default {0})",
+        default=DEFAULT_CONFIG)
+    parser.add_argument(
+        "-v", "--version", action="version", version=__version__)
+    args = parser.parse_args()
 
-# Parse config file
-if args.name:
-    config_parser = SafeConfigParser()
-    # Open config file
+    # Parse config file
+    if args.name:
+        config_parser = SafeConfigParser()
+        # Open config file
+        try:
+            config_parser.readfp(open(args.config, 'r'))
+        except IOError:
+            parser.error("config file '{}' not found".format(args.config))
+        # Section not found error
+        if not config_parser.has_section(args.name):
+            parser.error(
+                "section name '{}' not found in config file".format(args.name))
+        # Get options from config file (options from command line take
+        # precedence)
+        if not args.file and config_parser.has_option(args.name, 'file'):
+            args.file = config_parser.get(args.name, 'file')
+        if not args.dir and config_parser.has_option(args.name, 'dir'):
+            args.dir = config_parser.get(args.name, 'dir')
+
+    # More parse errors
+    if not args.file:
+        parser.error(
+            "'file' is required (you must set it via config file or cmd arg)")
+
+    # Default value of 'dir'
+    if not args.dir:
+        args.dir = "{0}.{1}".format(args.file, DEFAULT_DIR_EXT)
+
+    return args
+
+
+# Get all valid splitted files inside a dir
+#
+# This funtion skips all entries that are not files or end with some special
+# extensions.
+# An error message will be printed and the program will exit if any error
+# occur.
+# It returns a list will all splitted files.
+def _get_splitted(directory):
+    splitted_files = []
+
+    # Test all entries in the dir, getting the spllited files
     try:
-        config_parser.readfp(open(args.config, 'r'))
-    except IOError:
-        parser.error("config file '{}' not found".format(args.config))
-    # Section not found error
-    if not config_parser.has_section(args.name):
-        parser.error("section name '{}' not found in config file".format(args.name))
-    # Get options from config file (options from command line take precedence)
-    if not args.file and config_parser.has_option(args.name, 'file'):
-        args.file = config_parser.get(args.name, 'file')
-    if not args.dir and config_parser.has_option(args.name, 'dir'):
-        args.dir = config_parser.get(args.name, 'dir')
+        for entry in os.listdir(directory):
+            entry_path = os.path.join(directory, entry)
+            entry_is_valid = True
+            if not os.path.isfile(entry_path):
+                continue
+            for ext in IGNORE_FILES_EXT:
+                if entry.endswith(ext):
+                    entry_is_valid = False
+                    break
+            if entry_is_valid:
+                splitted_files.append(entry_path)
+    # Dir not found error
+    except OSError:
+        print("Error: Dir '{0}' not found".format(directory))
+        sys.exit(1)
+    # No splitted files error
+    if not splitted_files:
+        print("Error: No splitted files found in dir '{0}'".format(directory))
+        sys.exit(1)
 
-# Required 'file' error
-if not args.file:
-    parser.error("'file' is required (you must set it via config file or cmd arg)")
-
-# Default value of 'dir'
-if not args.dir:
-    args.dir = "{0}.{1}".format(args.file, DEFAULT_DIR_EXT)
-
-
-# Get all splitted files
-splitted_files = []
-try:
-    for entry in os.listdir(args.dir):
-        entry_path = os.path.join(args.dir, entry)
-        entry_is_valid = True
-        if not os.path.isfile(entry_path):
-            continue
-        for ext in IGNORE_FILES_EXT:
-            if entry.endswith(ext):
-                entry_is_valid = False
-                break
-        if entry_is_valid:
-            splitted_files.append(entry_path)
-except OSError:
-    print("Error: Dir '{0}' not found".format(args.dir))
-    sys.exit(1)
-# No splitted file error
-if not splitted_files:
-    print("Error: No splitted files found in dir '{0}'".format(args.dir))
-    sys.exit(1)
+    return splitted_files
 
 
-# Generate new config file
+# Create a temp config file
+#
+# This funtion merges all splited files into a new and temporary file.
+# It returns the path to the newly created temporary file.
+def _create_temp_config(splitted_files):
+    temp_file = None
 
-# Generate in a temp file
-temp_file_fd, temp_file = mkstemp()
-# Convert 'temp_file_fd' to a Python file object
-# See: http://www.logilab.org/blogentry/17873
-temp_file_fd = os.fdopen(temp_file_fd, 'w')
-for splitted in splitted_files:
-    with open(splitted, 'r') as splitted_fd:
-        temp_file_fd.write(splitted_fd.read())
-temp_file_fd.close()
+    temp_file_fd, temp_file = tempfile.mkstemp()
+    # Convert 'temp_file_fd' to a Python file object
+    # See: http://www.logilab.org/blogentry/17873
+    temp_file_fd = os.fdopen(temp_file_fd, 'w')
+    for splitted in splitted_files:
+        with open(splitted, 'r') as splitted_fd:
+            temp_file_fd.write(splitted_fd.read())
+    temp_file_fd.close()
 
-# Backup the old config file
-if os.path.isfile(args.file):
-    os.rename(args.file, "{0}.{1}".format(args.file, BACKUP_EXT))
+    return temp_file
 
-# Use the new config file
-# Using shutil.move because the tmp file can be in a different filesystem
-shutil.move(temp_file, args.file)
 
+# Move a temp config file to your final location
+#
+# This function makes a backup of the current config file and then puts the
+# new file (currently a temp file) to the final config file location.
+def _temp_to_file(temp_file, config_file):
+    # Backup
+    if os.path.isfile(config_file):
+        os.rename(config_file, "{0}.{1}".format(config_file, BACKUP_EXT))
+    # Move temp_file
+    # Using shutil.move because the tmp file can be in a different filesystem
+    shutil.move(temp_file, config_file)
+
+
+# Run the script
+def run():
+    args = _parse_all()
+    spplited_files = _get_splitted(args.dir)
+    temp_file = _create_temp_config(spplited_files)
+    _temp_to_file(temp_file, args.file)
+
+
+if __name__ == "__main__":
+    run()
