57f0c44413fdb88a0fd950089b2d5914a035b089
Rarylson Freitas <rarylson@vialink.com.br>
2015-03-01 21:05:35 -0300

    adding test to the _move_to_file function

---
+++ tests/move_tmp.py
@@ -0,0 +1,65 @@
+import sys
+import os
+from os.path import join, isfile
+from StringIO import StringIO
+import shutil
+import tempfile
+import filecmp
+
+import unittest
+
+import utils
+
+# App module
+app = utils.import_app()
+
+
+class MoveTempToConfigTest(unittest.TestCase):
+    """Tests of moving a config temp file to its final location
+    """
+
+    def setUp(self):
+        utils.clean_tmp()
+
+    def test_temp_to_file(self):
+        """Moves a temp file created to its final place
+        """
+        config_file = join(utils.TMP_DIR, "test1")
+        temp_file = join(tempfile.gettempdir(), "test1")
+        expected_file = join(utils.RESULTS_DIR, "test1")
+        # Simulating a temp file (in the system tmp dir)
+        shutil.copy(expected_file, temp_file)
+        try:
+            app._temp_to_file(temp_file, config_file)
+            self.assertTrue(
+                filecmp.cmp(config_file, expected_file, shallow=False))
+        finally:
+            if isfile(temp_file):
+                os.remove(temp_file)
+
+    def test_move_with_backup(self):
+        """App must make a backup of the current config when generating a
+        new one
+        """
+        config_file = join(utils.TMP_DIR, "test1")
+        temp_file = join(tempfile.gettempdir(), "test1")
+        expected_file = join(utils.RESULTS_DIR, "test1")
+        expected_file_bak = join(utils.RESULTS_DIR, "test2")
+        shutil.copy(expected_file, temp_file)
+        # Creating a previous config file
+        shutil.copy(expected_file_bak, config_file)
+        try:
+            app._temp_to_file(temp_file, config_file)
+            self.assertTrue(
+                filecmp.cmp(config_file, expected_file, shallow=False))
+            self.assertTrue(
+                filecmp.cmp(
+                    "{0}.bak".format(config_file), expected_file_bak,
+                    shallow=False))
+        finally:
+            if isfile(temp_file):
+                os.remove(temp_file)
+
+
+if __name__ == '__main__':
+    unittest.main()
